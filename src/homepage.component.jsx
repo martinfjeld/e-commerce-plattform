366cfae3ce87c0e7d4a32376b513f1578d6f32c0
martinfjeld <martinfjeld@live.no>
2021-08-14 16:02:20 +0200

    Created project and added files for homepage/ sass

---
+++ src/homepage.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+export const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">HATS</h1>
+            <span className="subtitle">BUY NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
